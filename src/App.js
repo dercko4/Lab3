5adb856947589c9f355630d3807e8272a1507041
dercko4 <dercko4@mail.ru>
2024-05-17 10:38:34 +0300

    Platform for App

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+function App() { 
+  return (
+    <div className="div-super">Hello world!</div>
+  ); 
+} 
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import App from './App';
+export const Context = createContext(null) 
+ 
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render( 
+  <React.StrictMode> 
+  <Context.Provider> 
+    <App/>
+  </Context.Provider>    
+  </React.StrictMode> 
+);
